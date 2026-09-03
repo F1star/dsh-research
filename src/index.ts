bee86263bb2413181da7ecc6b93b3100d876e40e
f1star <3201125193@qq.com>
2026-09-03 15:49:13 +0800

    feat: assemble the research profile bundle

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Public namespaces for the standalone DSH research bundle.
+ * @module @f1star/dsh-research
+ */
+
+export * as researchDocument from './research-document/index.ts'
+export * as researchDocumentPdfjs from './research-document-pdfjs/index.ts'
+export * as researchInformation from './research-information/index.ts'
+export * as researchLibrary from './research-library/index.ts'
+export * as researchDocumentTools from './tool-research-document/index.ts'
+export * as researchInformationTools from './tool-research-information/index.ts'
+export * as researchLibraryTools from './tool-research-library/index.ts'
